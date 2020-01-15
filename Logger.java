1831be13e836799665c654c677adc36b7db1b315
Omid <omidhazara@gmail.com>
2020-01-15 20:57:29 +0100

    Delete Logger.java

--- Logger.java
+++
@@ -1,8 +0,0 @@
-public class Logger {
-    private static final boolean ENABLE_LOGGING = true;
-    public static void log(String message) {
-        if (ENABLE_LOGGING) {
-            System.out.println(message);
-        }
-    }
-}
